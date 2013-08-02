365e01b077ef452c1f790a57242d9d284d429a5f
Casey Dierking <hnwebdesign5@gmail.com>
2013-08-02 13:32:19 +0700

    Fixing borked migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130720073141) do
+ActiveRecord::Schema.define(:version => 20130802042528) do
 
   create_table "forem_categories", :force => true do |t|
     t.string   "name",       :null => false
@@ -148,10 +148,12 @@ ActiveRecord::Schema.define(:version => 20130720073141) do
     t.boolean  "forem_admin",            :default => false
     t.string   "forem_state",            :default => "pending_review"
     t.boolean  "forem_auto_subscribe",   :default => false
+    t.string   "slug"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["slug"], :name => "index_users_on_slug"
 
   create_table "users_roles", :id => false, :force => true do |t|
     t.integer "user_id"
