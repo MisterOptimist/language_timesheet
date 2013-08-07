98dea076053d0a9ed3e339b5ba802f3a1472ca8e
Casey Dierking <hnwebdesign5@gmail.com>
2013-08-07 08:27:07 +0700

    Added styling

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,11 +4,11 @@ class ActivitiesController < ApplicationController
 
   def index
   	@users = User.all
-  	@activities = PublicActivity::Activity.order("created_at desc")
+  	@activities = PublicActivity::Activity.order("created_at desc").page(params[:page]).per(7)
   	
   end
 
 
 
-
+ 
 end
