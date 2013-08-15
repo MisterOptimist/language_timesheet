f2688412e0ca7ecf83f82c419347d155e78d3d61
Casey Dierking <hnwebdesign5@gmail.com>
2013-08-15 14:58:51 +0700

    blank

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
 
 
   def index
-  	@users = User.all
+  	@users = User.all 
   	@activities = PublicActivity::Activity.order("created_at desc").page(params[:page]).per(7)
   	
   end
