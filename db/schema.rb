fdcc2fc50cffe2c9e325e9785dd923b98bb8f50f
Casey Dierking <hnwebdesign5@gmail.com>
2013-08-20 15:08:43 +0700

    Added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820044604) do
+ActiveRecord::Schema.define(:version => 0) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -187,7 +187,6 @@ ActiveRecord::Schema.define(:version => 20130820044604) do
     t.datetime "updated_at",      :null => false
     t.text     "khmersentence"
     t.text     "englishsentence"
-    t.text     "phonetic"
   end
 
 end
