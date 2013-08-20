50cdac1faa95ff0fea93f673665af3d2a9928d67
Casey Dierking <hnwebdesign5@gmail.com>
2013-08-20 22:01:55 +0700

    Added date joined staff column and added it to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :joined_staff
 
 
  #When user signs up, automatically assigns them as Staff
