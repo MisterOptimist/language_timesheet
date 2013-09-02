f4e0da1b39709196ad8a085c08a511c7dce75834
Casey Dierking <hnwebdesign5@gmail.com>
2013-09-02 13:53:59 +0700

    Fixed mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ TimesheetApp::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://bblanguageteam.com' }
 
-        config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.delivery_method = :smtp
 
         config.action_mailer.smtp_settings = {
         address: "smtp.gmail.com",
@@ -79,7 +79,7 @@ TimesheetApp::Application.configure do
 
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
