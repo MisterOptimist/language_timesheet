84614f45e7ae2df6f12afcea8d2d66f0f01b9b17
Casey Dierking <hnwebdesign5@gmail.com>
2013-09-02 16:18:03 +0700

    fixed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ TimesheetApp::Application.configure do
   config.active_support.deprecation = :notify
 
 
-   config.action_mailer.default_url_options = { :host => 'http://bblanguageteam.com' }
+   config.action_mailer.default_url_options = { :host => 'bblanguageteam.com' }
 
 #   config.action_mailer.delivery_method = :smtp
 
