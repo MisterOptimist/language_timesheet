8ab8985995ec497c4568535e235c34494169d812
Casey Dierking <hnwebdesign5@gmail.com>
2013-09-10 20:03:47 +0700

    refactored tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,18 +7,15 @@ describe User do
 	end
   
 	it "is invalid without a name" do 
-		user = FactoryGirl.build(:user, name: nil)
-		expect(user).to have(1).errors_on(:name)
+		expect(build(:user, name: nil)).to have(1).errors_on(:name)
 	end
 
 	it "is invalid without an email" do
-		user = FactoryGirl.build(:user, email: nil)
-		expect(user).to have(2).errors_on(:email)
+		expect(build(:user, email: nil)).to have(2).errors_on(:email)
 	end
 
 	it "is invalid without a date staffed date" do 
-		user = FactoryGirl.build(:user, joined_staff: nil)
-		expect(user).to have(1).errors_on(:joined_staff)
+		expect(build(:user, joined_staff: nil)).to have(1).errors_on(:joined_staff)
 	end
 
 	it "is invalid with a duplicate email address" do 
@@ -26,4 +23,6 @@ describe User do
 		FactoryGirl.create(:user, email: "hello@gmail.com")
 		expect(user).to have(1).errors_on(:email)
 	end
+
+
 end
