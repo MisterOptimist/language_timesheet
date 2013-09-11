ba1b78f1a6eca624a2617a6fccee8b33f2ff955d
Casey Dierking <hnwebdesign5@gmail.com>
2013-09-11 17:08:48 +0700

    Added test for user to view word of the day

--- spec/features/words_spec.rb
+++ spec/features/words_spec.rb
@@ -8,11 +8,28 @@ feature 'Word management' do
 		click_link 'Log In'
 		fill_in 'Email', with: admin.email
 		fill_in 'Password', with: admin.password
+		click_button('Sign in')
+
+		expect(page).to have_content "Welcome"
+		click_link('Manage Word of the Day')
+		expect(page).to have_content "Edit"
+	end
+
+	scenario "can view a new word" do 
+		user = create(:user)
 
 		visit root_path
-		expect{
-			click_link 'Manage Word of the Day'
-			click_link 'Add a New Word'
-		}
+		click_link 'Log In'
+		fill_in 'Email', with: user.email
+		fill_in 'Password', with: user.password
+		click_button('Sign in')
+
+		expect(page).to have_content "Welcome"
+		
+		expect(page).to_not have_content "Manage Word of the Day"
+		click_link('Word of the Day')
+		expect(page).to have_content "Listing Words"
+
 	end
+
 end
